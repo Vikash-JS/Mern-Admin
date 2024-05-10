d923edf3fb7dc41c2b54b72b64650e42f00cad6c
Vikash-JS <vikash9229sharma@gmail.com>
2024-05-10 22:08:19 +0530

    Create Client for mern-admin project

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500">Mern Admin</h1>
+    </div>
+  );
+}
+
+export default App;
