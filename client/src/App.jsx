d66dfeabf111481b753585253e09e3cc6051a071
Vikash-JS <vikash9229sharma@gmail.com>
2024-05-10 23:02:53 +0530

    Added the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
